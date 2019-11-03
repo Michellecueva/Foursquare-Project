fc6997d917dd04d04608b478322c4baecd04a474
Michelle <Michelle.cueva@gmail.com>
2019-11-03 17:32:24 -0500

    Creates tab bar controller programmatically, renames SearchVC, Creates Collections VC

---
+++ Foursquare-Project/CollectionVC.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionVC.swift
+//  Foursquare-Project
+//
+//  Created by Michelle Cueva on 11/3/19.
+//  Copyright © 2019 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class CollectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+
+    }
+
+}
--- Foursquare-Project/SceneDelegate.swift
+++ Foursquare-Project/SceneDelegate.swift
@@ -17,7 +17,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = createMainTabBarController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -47,6 +51,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
+    
+    
+    private func createMainTabBarController() -> UITabBarController {
+           let firstvc = UINavigationController.init(rootViewController: SearchVC())
+           let secondvc = UINavigationController.init(rootViewController: CollectionVC())
+        
+           firstvc.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+           secondvc.tabBarItem = UITabBarItem(title: "Collections", image: UIImage(systemName: "rectangle.stack"), tag: 1)
+           let tabVC = UITabBarController()
+           tabVC.setViewControllers([firstvc,secondvc], animated: false)
+           return tabVC
+       }
 
 
 }
